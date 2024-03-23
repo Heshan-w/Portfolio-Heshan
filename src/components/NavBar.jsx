d61c52a53efbae798dfbdd773dfab4e00b0458ec
Heshan-w <htwanigasinghe@gmail.com>
2024-03-23 19:07:02 +0530

    refactor: changed "About Me" link to "Home" and added a Tooltip to GitHub link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,26 @@
 import { Link } from "react-router-dom";
+import { Tooltip } from "@nextui-org/react";
 
 const NavBar = () => {
   return (
     <div className="flex flex-col sm:flex-row justify-between items-center bg-black border-b-2 font-mono border-neutral-200 sticky top-0 z-50 py-6 px-4 sm:px-0">
       <div className="text-xl text-neutral-200 px-4 sm:px-12">
-        <a href="https://github.com/Heshan-w" target="blank">
-          Heshan-w
-        </a>
+        <Tooltip
+          content="GitHub"
+          placement="bottom"
+          showArrow={true}
+          className="bg-neutral-200"
+        >
+          <a href="https://github.com/Heshan-w" target="blank">
+            Heshan-w
+          </a>
+        </Tooltip>
       </div>
 
       <div className="flex flex-col sm:flex-row text-neutral-200 text-xl ">
         {/* Link component is used to create a link to a different route in the application */}
         <Link to="/" className="px-4 sm:px-12 hover:underline">
-          About Me
+          Home
         </Link>
         <Link to="/projects" className="px-4 sm:px-12 hover:underline">
           Projects
