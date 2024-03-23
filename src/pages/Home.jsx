f0069cfb8c078654e3f8385c0386ae045a8ffdd7
Heshan-w <htwanigasinghe@gmail.com>
2024-03-23 19:11:09 +0530

    style: changed scroll button position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -147,7 +147,7 @@ const Home = () => {
         >
           <div
             onClick={scrollToTop}
-            className="fixed bottom-5 right-5 cursor-pointer p-2 mr-7"
+            className="fixed bottom-5 right-5 cursor-pointer p-1 mr-1"
           >
             <img
               src="/assets/upArrow.png"
