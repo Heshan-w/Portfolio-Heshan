a429b14917a42cd7f5063a7ae73212eae338ed2e
Heshan-w <htwanigasinghe@gmail.com>
2024-03-25 23:24:56 +0530

    refactor: update TechCard component styling

--- src/components/TechCard.jsx
+++ src/components/TechCard.jsx
@@ -5,12 +5,12 @@ const TechCard = ({ image, name }) => {
   return (
     <div className="font-mono p-3">
       <Card shadow="none" className="bg-black">
-        <CardBody className="h-[130px] md:h-[150px] w-[130px] md:w-[150px] flex justify-center items-center rounded-md border-black">
+        <CardBody className="h-[120px] md:h-[130px] w-[120px] md:w-[130px] flex justify-center items-center rounded-md border-black">
           <div className="flex flex-col justify-center items-center text-center border-black">
             <img
               src={image}
               alt="technology logo"
-              className="w-[65px] md:w-[75px] h-[65px] md:h-[75px]"
+              className="w-[40px] md:w-[65px] h-[40px] md:h-[65px]"
             />
             <p className="text-neutral-200 mt-2">{name}</p>
           </div>
